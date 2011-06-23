1b0c072278bc14655c2381edeb792f0ad6a718c4
Jun Namikawa <jnamika@gmail.com>
2011-06-23 15:04:46 +0900

    modified code to check default argument values

--- attrcheck.py
+++ attrcheck.py
@@ -5,18 +5,22 @@ import inspect
 def attrcheck(*arguments, **keywords):
     errmsg = "Error checking argument %r of function %r with hasattr(%r, '%s')"
     def _(f):
+        def check(k, v):
+            if k in keywords:
+                for n in keywords[k]:
+                    if not hasattr(v, n):
+                        raise AttributeError(errmsg % (k, f.__name__, v, n))
         spec = inspect.getargspec(f)
+        if spec.defaults != None:
+            n = len(spec.args) - len(spec.defaults)
+            for i,v in enumerate(spec.defaults):
+                check(spec.args[n+i], v)
         def __(*args, **kw):
-            def check(k):
-                if k in keywords:
-                    for n in keywords[k]:
-                        if not hasattr(v, n):
-                            raise AttributeError(errmsg % (k, f.__name__, v, n))
             for i,v in enumerate(args):
                 k = spec.args[i]
-                check(k)
+                check(k, v)
             for k,v in kw.iteritems():
-                check(k)
+                check(k, v)
             return f(*args, **kw)
         return __
     return _
